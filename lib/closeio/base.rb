5c9a6a2bbb3539019e0742cc1756c452c6fae1f5
Leandro Ostera <me@leostera.com>
2014-06-24 23:25:31 +0200

    use basic_auth with single 'token:' string

--- lib/closeio/base.rb
+++ lib/closeio/base.rb
@@ -3,7 +3,7 @@ module Closeio
 
     include HTTParty
     base_uri   'https://app.close.io/api/v1'
-    basic_auth ENV['CLOSEIO_API_KEY'], ''
+    basic_auth "#{ENV['CLOSEIO_API_KEY']}:"
     headers 'Content-Type' => 'application/json'
     debug_output $stdout
     format :json
